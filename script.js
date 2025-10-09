1f08cbb8d4efe88d9c4b17d0efba0dc5040f4163
joeesmithh <josmth3030@gmail.com>
2025-10-09 15:04:28 -0400

    feat: darken square color on mouse over

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+function changeColor(elem) {
+    let o = parseFloat(elem.style.opacity);
+    elem.style.opacity = `${o + 0.1}`;
+}
+
 // Body
 let body = document.querySelector("body");
-body.setAttribute("style",  `
+body.setAttribute("style", `
     width: 45%;
     height: 100vh;
     margin: auto; 
-    align-content: center;    
-    justify-content: center;    
-    align-items: center;
-    align: center;
+    align-content: center;      
 `);
 
 // Grid
@@ -16,24 +18,37 @@ grid.setAttribute("class", "grid");
 grid.setAttribute("style", `
     width: 100%;
     aspect-ratio: 1 / 1;
+    border-style: solid;
+    border-width: 1px;
+
     display: flex;
     justify-content: flex-start;
     flex-wrap: wrap;
 `);
 
-// Squares
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.setAttribute("class", "square");
-    square.setAttribute("style", `
-        width: 6.25%;
-        height: 6.25%;
-        border-style: solid;
-        border-color: black;
-        border-width: 1px;
-    `);
-    grid.appendChild(square);
+grid.addEventListener('mouseover', (e) => {
+    changeColor(e.target);
+})
 
+// Squares
+const grid_size = 16;
+let square_count = grid_size * grid_size;
+let square_size = 100 / grid_size;
+for (let i = 0; i < square_count; i++) {
+    let square = document.createElement('div');
+    square.setAttribute(`id`, `square${i}`);
+    square.setAttribute("style", `
+        width: ${square_size}%;
+        height: ${square_size}%;
+        background-color: black;
+        opacity: 0;
+    `);
+
+    // square.addEventListener('mouseenter', (e) => {
+    //     changeColor(e.target);
+    // });
+
+    grid.appendChild(square);
 }
 
 // Add grid
